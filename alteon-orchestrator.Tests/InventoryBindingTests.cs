30524d3b8996d0e8948b6580c2e1e396eabbf8fd
Joe VanWanzeele <jvanwanzeele@keyfactor.com>
2026-04-22 14:24:29 -0400

    cleanup

--- alteon-orchestrator.Tests/InventoryBindingTests.cs
+++ alteon-orchestrator.Tests/InventoryBindingTests.cs
@@ -9,7 +9,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer.Jobs;
 using Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer.Tests.Helpers;
