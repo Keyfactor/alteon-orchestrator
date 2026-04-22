03b2b709c2ae58d931ee2e1e6ae622c01fa7de22
Morgan Gangwere <470584+indrora@users.noreply.github.com>
2026-04-22 10:44:56 -0700

    Merge 1.2.0 to main (#15)
    * [skip ci] updating the github action on the release-1.2 branch (#14)
    
    * Add Keyfactor Release Workflow configuration
    
    * Delete .github/workflows/keyfactor-starter-workflow.yml
    
    * Update generated docs
    
    ---------
    
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    
    * Apply and save actions after certificate operations (#13)
    
    * Prep for 1.0 release (#6)
    
    * Added Store Type Advanced tab values to README. fixed typo.
    
    * Added server credentials screen shot and details.
    
    * add support level to manifest for readme
    
    * Update generated README
    
    ---------
    
    Co-authored-by: Michael Henderson <mhenderson@keyfactor.com>
    Co-authored-by: JoeKF <jvanwanzeele@keyfactor.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    
    * [skip ci] (#7)
    
    update manifest for public
    
    * Switched order of updating cert & key from cert then key to key then cert to fix error from API.
    
    * added changelog
    
    * Create keyfactor-bootstrap-workflow.yml
    
    * Update generated README
    
    * updated manifest with minimum platform version
    
    * added UOFramework property for minimum UO framework supported
    
    * Update generated README
    
    * Added release_dir to manifest
    
    * Update generated README
    
    * updated manifest, added additional logging, and wrapped private key in PEM headers before submitting.
    
    * utilizing the Keyfactor.PKI libraries for cert processing
    
    * Merged existing
    
    * Now checking for existing cert with alias.  If exists; passing renew=1 flag for the API to handle correctly.
    
    * updated changelog
    
    * Added method for apply and save
    
    * updated github action
    
    * updated integration-manifest to v2
    
    * Update generated docs
    
    * updated manifest
    
    * Update generated docs
    
    * Update generated docs
    
    * updated changelog
    
    * updated license headers
    
    * updated license headers, cleanup
    
    * Update generated docs
    
    * updated to check item type when looking for existing entries
    
    * Update generated docs
    
    * added delay in order to wait for MP to stabalize when logging the apply table
    
    * added delay to wait for MP to stabilize before getting apply table.
    
    ---------
    
    Co-authored-by: Mikey Henderson <Michael.Henderson@keyfactor.com>
    Co-authored-by: Michael Henderson <mhenderson@keyfactor.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    
    ---------
    
    Co-authored-by: Joe VanWanzeele <76071503+joevanwanzeeleKF@users.noreply.github.com>
    Co-authored-by: Keyfactor <keyfactor@keyfactor.github.io>
    Co-authored-by: Mikey Henderson <Michael.Henderson@keyfactor.com>
    Co-authored-by: Michael Henderson <mhenderson@keyfactor.com>

--- alteon-orchestrator/AlteonLoadBalancerClient.cs
+++ alteon-orchestrator/AlteonLoadBalancerClient.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2022 Keyfactor
+﻿// Copyright 2026 Keyfactor
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Keyfactor.Logging;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using RestSharp;
 using RestSharp.Authenticators;
 
@@ -26,37 +27,56 @@ namespace Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer
     public class AlteonLoadBalancerClient
     {
         private RestClient _restClient { get; set; }
-        ILogger logger = LogHandler.GetClassLogger<AlteonLoadBalancerClient>();
+        protected ILogger logger { get; set; }
 
-        public AlteonLoadBalancerClient(string baseUrl, string username, string password)
+        public AlteonLoadBalancerClient(string baseUrl, string username, string password, ILogger logger)
         {
+            this.logger = logger;
+
             var options = new RestClientOptions(baseUrl)
             {
-                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true, // this is to allow self-signed appliance certs
+                Authenticator = new HttpBasicAuthenticator(username, password)
             };
             _restClient = new RestClient(options);
-
-            _restClient.Authenticator = new HttpBasicAuthenticator(username, password);
         }
 
         public async Task<CertificateTableEntryCollection> GetCertificates()
         {
-            var request = new RestRequest(Endpoints.CertificateRepository);
+            logger.MethodEntry();
+
+            var request = new RestRequest(Endpoints.CertificateRepository, Method.Get);
+
+            logger.LogTrace($"making request to retrieve certificates from endpoint: {request.Resource}");
             try
             {
-                var response = await _restClient.GetAsync<CertificateTableEntryCollection>(request);
-                return response;
+                var response = await _restClient.ExecuteAsync(request);
+                if (!response.IsSuccessful) {
+                    logger.LogTrace($"the request failed with status code {response.StatusCode} and error message:");
+                    logger.LogTrace($"{response.ErrorMessage}");
+                    logger.LogTrace($"{response.Content}");
+                }
+                var certs = JsonConvert.DeserializeObject<CertificateTableEntryCollection>(response.Content);
+                return certs;
             }
             catch (Exception ex)
             {
+                logger.LogError($"An error occurred when attempting to retrieve the certificates from {_restClient.BuildUri(request)?.ToString()}");
                 logger.LogError(ex.Message, ex);
                 throw;
             }
+            finally
+            {
+                logger.MethodExit();
+            }
         }
 
-        public async Task<CertificateTableEntryCollection> GetCertificatesById(string id)
+        public async Task<CertificateTableEntryCollection> GetCertificatesById(string id, string type)
         {
-            var url = $"{Endpoints.CertificateRepository}?filter=ID:{id}&filtertype=exact&props=ID,Type";
+            logger.MethodEntry();
+            var typeInt = AlteonCertTypes.AlteonCertTypeValue(type);
+
+            var url = $"{Endpoints.CertificateRepository}?filter=ID:{id},Type:{typeInt}&filtertype=exact&props=ID,Name,Type";
             var request = new RestRequest(url);
 
             // the filter above _should_ return only the certs and keys with that alias.  
@@ -64,8 +84,20 @@ namespace Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer
 
             try
             {
-                var collection = await _restClient.GetAsync<CertificateTableEntryCollection>(request);
-                collection.SlbNewSslCfgCertsTable = collection.SlbNewSslCfgCertsTable.FindAll(c => c.ID == id);
+                logger.LogTrace($"retreiving certs from API endpoint: {url}");
+                var response = await _restClient.GetAsync(request);
+                if (!response.IsSuccessStatusCode)
+                {
+                    logger.LogTrace($"the request failed with status code {response.StatusCode} and error message:");
+                    logger.LogTrace($"{response.ErrorMessage}");
+                    logger.LogTrace($"{response.Content}");
+                    throw response.ErrorException;
+                }
+
+                var collection = JsonConvert.DeserializeObject<CertificateTableEntryCollection>(response.Content);
+
+                //var collection = await _restClient.GetAsync<CertificateTableEntryCollection>(request);
+                collection.SlbNewSslCfgCertsTable = collection.SlbNewSslCfgCertsTable.FindAll(c => c.ID == id && c.Type == typeInt);
                 return collection;
             }
             catch (Exception ex)
@@ -73,13 +105,19 @@ namespace Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer
                 logger.LogError(ex.Message, ex);
                 throw;
             }
+            finally { logger.MethodExit(); }
         }
 
         public string GetCertificateContent(string certId)
         {
+            logger.MethodEntry();
             var request = new RestRequest(Endpoints.CertificateContent);
             request.AddQueryParameter("id", certId);
             request.AddQueryParameter("type", "srvcrt");
+            var fullUri = _restClient.BuildUri(request);
+
+            logger.LogTrace($"making request to get certificate from the endpoint: {fullUri}");
+
             try
             {
                 var response = _restClient.DownloadData(request);
@@ -89,20 +127,39 @@ namespace Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer
             }
             catch (Exception ex)
             {
+                logger.LogError($"An error occurred when attempting to retrieve the certificate with id '{certId}' from {fullUri}");
                 logger.LogError(ex.Message, ex);
                 throw;
             }
+            finally { logger.MethodExit(); }
         }
 
-        public async Task AddCertificate(string alias, string pfxPassword, string certContents, string type)
+        public async Task AddCertificate(string alias, string pfxPassword, string certContents, string type, bool overwrite)
         {
+            logger.MethodEntry();
+            // first, see if a certificate with this alias/id already exists
+            logger.LogTrace($"checking to see if an entry with alias '{alias}' and type '{type}' exists..");
+            var existing = await GetCertificatesById(alias, type);
+            var replace = false;
+            if (existing.SlbNewSslCfgCertsTable?.Count > 0)
+            {
+                logger.LogTrace("it does..");
+                // the cert already exists; if overwrite == true, we should overwrite; else exist here.
+                if (!overwrite) throw new Exception($"The certificate with id {alias} already exists and Overwrite == false.");
+                replace = true; // if it exists and overwrite is true, we replace it.
+                logger.LogTrace(".. and overwrite is true, passing renew=1 in the query.");
+            }
+            else logger.LogTrace("..it does not.  Adding as new");
+
             var request = new RestRequest(Endpoints.AddCertificate, Method.Post);
             request.AddQueryParameter("id", alias);
             request.AddQueryParameter("type", type);
             request.AddQueryParameter("passphrase", pfxPassword);
             request.AddQueryParameter("src", "txt");
-
+            if (replace) request.AddQueryParameter("renew", 1);
             request.AddBody(certContents);
+            var fullUri = _restClient.BuildUri(request);
+            logger.LogTrace($"posting certificate to the uri {fullUri}");
 
             try
             {
@@ -117,34 +174,161 @@ namespace Keyfactor.Extensions.Orchestrator.AlteonLoadBalancer
                 logger.LogError(ex.Message, ex);
                 throw;
             }
+            finally
+            {
+                logger.MethodExit();
+            }
         }
 
         internal async Task RemoveCertificate(string alias)
         {
-            var existing = (await GetCertificatesById(alias)).SlbNewSslCfgCertsTable;
+            logger.MethodEntry();
+            var url = string.Empty;
+
+            var existing = (await GetCertificatesById(alias, AlteonCertTypes.CERT_ONLY)).SlbNewSslCfgCertsTable;
             if (existing.Count == 0)
             {
                 throw new Exception($"Certificate with alias {alias} not found.");
             }
             try
             {
-                existing.ForEach(c =>
+                foreach (var c in existing)
                 {
-                    var url = $"{Endpoints.CertificateRepository}/{c.ID}/{c.Type}";
+                    url = $"{Endpoints.CertificateRepository}/{c.ID}/{c.Type}";
                     var request = new RestRequest(url, Method.Delete);
-
-                    var response = _restClient.DeleteAsync(request).Result;
+                    var fullUri = _restClient.BuildUri(request);
+                    logger.LogTrace($"making request to remove certificate to uri {fullUri}");
+                    var response = await _restClient.DeleteAsync(request);
 
                     if (!response.IsSuccessful)
                     {
                         throw new Exception($"Failed to remove certificate: {alias}", response.ErrorException);
                     }
-                });
+                }
+                // apply and save changes
+                await ApplyAndSave();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"An error occurred when attempting to remove the certificate with alias {alias} via endpoint: '{url}'");
+                logger.LogError(ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                logger.MethodExit();
+            }
+        }
+
+        /// <summary>
+        /// This method is intended to be called after making changes to the certs/keys on the Alteon.  It will apply the changes and save the config, so that the changes persist through a reboot.  If this isn't called after making changes, the changes will be lost on reboot.
+        /// </summary>
+        /// <returns></returns>
+        internal async Task ApplyAndSave()
+        {
+            logger.MethodEntry();
+            logger.LogTrace($"making requests to apply and save changes");
+            try
+            {
+                await ApplyChanges();
+                await Task.Delay(3000); // wait for MP to stabilize after apply
+                await LogApplyTable();
+                await SaveChanges();
             }
             catch (Exception ex)
             {
                 logger.LogError(ex.Message, ex);
                 throw;
+            }
+            finally
+            {
+                logger.MethodExit();
+            }
+        }
+
+        internal async Task ApplyChanges()
+        {
+            logger.MethodEntry();
+
+            var applyRequest = new RestRequest(Endpoints.Config, Method.Post);
+            applyRequest.AddQueryParameter("action", "apply");
+            applyRequest.AddHeader("Accept", "application/json");
+
+            var fullUri = _restClient.BuildUri(applyRequest);
+            logger.LogTrace($"making request to apply and save changes to {fullUri.ToString()}");
+            try
+            {
+                var response = await _restClient.ExecuteAsync(applyRequest);
+                if (!response.IsSuccessful)
+                {
+                    logger.LogError($"request to apply changes failed with error message: {response.ErrorMessage}\ncontent:{response.Content}");
+                    throw new Exception($"Failed to apply changes.", response.ErrorException);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"request to apply changest failed: {ex.Message}", ex);
+                throw;
+            }
+            finally
+            {
+                logger.MethodExit();
+            }
+        }
+
+        internal async Task SaveChanges()
+        {
+            logger.MethodEntry();
+            var saveRequest = new RestRequest(Endpoints.Config, Method.Post);
+            saveRequest.AddQueryParameter("action", "save");
+            saveRequest.AddHeader("Accept", "application/json");
+
+            var fullUri = _restClient.BuildUri(saveRequest);
+            logger.LogTrace($"making request to save changes to {fullUri.ToString()}");
+            try
+            {
+                var response = await _restClient.ExecuteAsync(saveRequest);
+                if (!response.IsSuccessful)
+                {
+                    logger.LogError($"request to save changes failed: {response.ErrorMessage}, {response.Content}");
+                    throw new Exception($"Failed to save changes, status code : {response.StatusCode}\nerror message : {response.ErrorMessage}\ncontent:{response.Content}");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                logger.MethodExit();
+            }
+        }
+
+        internal async Task LogApplyTable()
+        {
+            logger.MethodEntry();
+            var request = new RestRequest(Endpoints.ApplyTable, Method.Get);
+            request.AddHeader("Accept", "application/json");
+            var fullUri = _restClient.BuildUri(request);
+            logger.LogTrace($"making request to get apply table to uri {fullUri}");
+            try
+            {
+                var response = await _restClient.ExecuteAsync(request);
+                if (!response.IsSuccessful) { 
+                    logger.LogError($"There was an error getting the pending changes: {response.StatusCode}\t{response.ErrorMessage}\t{response.Content}");
+                    throw response.ErrorException;
+                }
+                logger.LogTrace($"Apply table response: {response.Content}");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"LogApplyTable failed (not critical) - {ex.Message}", ex);
+                // continuing.. non-critical
+            }
+            finally
+            {
+                logger.MethodExit();
             }
         }
     }
